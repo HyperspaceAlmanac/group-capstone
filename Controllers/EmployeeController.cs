3b3cec9da3e02963ec3c23a3996caccd2c8dde7e
Jacob Werner <jacob.scott.werner@gmail.com>
2021-04-13 15:03:38 -0500

    Adds CRUD functionality to EmployeeController (adds views for each).

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -27,5 +27,187 @@ namespace CarRentalService.Controllers
         {
             _context = context;
         }
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var employee = await _context.Employees.Where(c => c.IdentityUserId == userId).SingleOrDefaultAsync();
+            if (!employee.CompletedRegistration)
+            {
+                return RedirectToAction(nameof(Edit));
+            }
+            else
+            {
+                //var issue = await _context.Trips.Where(trip => trip.EmployeeId == employee.Id && trip.EndTime == null).SingleOrDefaultAsync();
+                //if (trip == null)
+                //{
+                   return RedirectToAction("SelectVehicle");
+                //}
+                //else
+                //{
+                //    return RedirectToAction(nameof(IssuePage));
+                //}
+            }
+            
+        }
+        public async Task<ActionResult> SelectVehicle()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var employee = _context.Employees.Where(c => c.IdentityUserId == userId).SingleOrDefault();
+            var vehicles = _context.Vehicles.Where(v => v.IsAvailable == true).ToList();
+            string employeeLocation = employee.CurrentLat.ToString() + ',' + employee.CurrentLong.ToString();
 
+            //Save employee location a GeoCode request.
+
+            for (var i = 0; i < vehicles.Count(); i++)
+            {
+                var geoCodingEngine = GoogleMaps.Geocode;
+                GeocodingRequest geocodeRequest = new GeocodingRequest()
+                {
+                    Address = $"{vehicles[i].CurrentStreet}, {vehicles[i].CurrentCity}, {vehicles[i].CurrentState} {vehicles[i].CurrentZip}",
+                    ApiKey = Secrets.GOOGLE_API_KEY,
+                };
+                GeocodingResponse geocode = await geoCodingEngine.QueryAsync(geocodeRequest);
+                vehicles[i].LastKnownLatitude = geocode.Results.First().Geometry.Location.Latitude;
+                vehicles[i].LastKnownLongitude = geocode.Results.First().Geometry.Location.Longitude;
+                _context.Update(vehicles[i]);
+                vehicles[i].Location = vehicles[i].LastKnownLatitude.ToString() + ',' + vehicles[i].LastKnownLongitude.ToString();
+
+                string url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + employeeLocation + "&destination=" + vehicles[i].Location + "&key=" + Secrets.GOOGLE_API_KEY;
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(url);
+                string jsonResult = await response.Content.ReadAsStringAsync();
+                JObject jobject = JObject.Parse(jsonResult);
+                int distanceLength = jobject.SelectToken("routes[0].legs[0].distance.text").ToString().Length;
+                double distance = Convert.ToDouble(jobject.SelectToken("routes[0].legs[0].distance.text").ToString().Substring(0, distanceLength - 3));
+                vehicles[i].Distance = distance;
+
+                int durationLength = jobject.SelectToken("routes[0].legs[0].duration.text").ToString().Length;
+                double duration = Convert.ToDouble(jobject.SelectToken("routes[0].legs[0].duration.text").ToString().Substring(0, durationLength - 5));
+                vehicles[i].Duration = duration;
+            }
+            await _context.SaveChangesAsync();
+            List<Vehicle> vehiclesSorted = vehicles.OrderBy(v => v.Distance).ToList();
+            return View(vehiclesSorted);
+        }
+        public async Task<IActionResult> Edit()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var employee = await _context.Employees.Where(c => c.IdentityUserId == userId).SingleOrDefaultAsync();
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Models.Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    GeocodingRequest geocodeRequest = new GeocodingRequest()
+                    {
+                        Address = $"{employee.CurrentStreet}, {employee.CurrentCity}, {employee.CurrentState} {employee.CurrentZip}",
+                        ApiKey = Secrets.GOOGLE_API_KEY,
+                        SigningKey = "Lew Vine"
+                    };
+
+                    var geoCodingEngine = GoogleMaps.Geocode;
+                    GeocodingResponse geocode = await geoCodingEngine.QueryAsync(geocodeRequest);
+                    employee.CurrentLat = geocode.Results.First().Geometry.Location.Latitude;
+                    employee.CurrentLong = geocode.Results.First().Geometry.Location.Longitude;
+                    employee.CompletedRegistration = true;
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", employee.IdentityUserId);
+            return View(employee);
+        }
+
+        public async Task<IActionResult> RegisterAccount(Models.Employee employee)
+        {
+            _context.Add(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Models.Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", employee.IdentityUserId);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(c => c.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
     }
+}
