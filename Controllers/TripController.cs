f67b0a20cdceded35c8cd986c18d0dfd8ca2a9ce
Almanac <ruoyw0@gmail.com>
2021-04-12 22:16:00 -0700

    Added in javascript support by using site.js and including it

---
+++ Controllers/TripController.cs
@@ -0,0 +1,59 @@
+﻿using CarRentalService.Data;
+using CarRentalService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarRentalService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public TripController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<TripController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.Trips.FindAsync();
+            return Ok(result);
+        }
+
+        // GET api/<TripController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return NotFound();
+        }
+
+        // POST api/<TripController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // PUT api/<TripController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // PUT api/UpdateTrip/<TripController>/5
+        [HttpPut("EndDestination/{id}")]
+        public IActionResult FillForm(int id, [FromForm] string value)
+        {
+            return Ok();
+        }
+
+    }
+}
