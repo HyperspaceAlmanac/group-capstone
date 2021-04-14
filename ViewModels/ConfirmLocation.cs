6e6028b320e1586e2c4f2e7dec2c914357c3b780
Almanac <ruoyw0@gmail.com>
2021-04-13 23:06:59 -0700

    Updating models for backend API

---
+++ ViewModels/ConfirmLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalService.ViewModels
+{
+    public class ConfirmLocation
+    {
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public string Location { get; set; }
+    }
+}
